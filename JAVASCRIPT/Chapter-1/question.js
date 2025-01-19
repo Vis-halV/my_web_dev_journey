685c638f52e93c00a423f3bde2458c173d6cfd27
Vis-halV <vishal06076@gmail.com>
2025-01-19 19:16:11 +0530

    Practice question

---
+++ JAVASCRIPT/Chapter-1/question.js
@@ -0,0 +1,19 @@
+// Qs1. Create a const object called "Product" to store information in the picture.
+
+const Product = {
+    title : "Ball Pen",
+    rating : 4,
+    isDeal : true,
+    offer : 5,
+    price : 270
+};
+
+// Qs2. Create a const object called "Profile" to store information showm in picture.
+
+const Profile = {
+    userName : "@vis.hal007",
+    isFollow : true,
+    followers : 276,
+    following : 329
+};
+
