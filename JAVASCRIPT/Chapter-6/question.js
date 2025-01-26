a27ac228d103337364a8113fd71e40b95fe71916
Vis-halV <vishal06076@gmail.com>
2025-01-26 15:32:17 +0530

    Practice questions

---
+++ JAVASCRIPT/Chapter-6/question.js
@@ -0,0 +1,17 @@
+/* Qs1. Create a H2 heading with text - "Hello JavaScript". 
+    Append "from Visanth Designs" to this text using JS. */
+
+let h2 = document.querySelector("h2");
+console.dir(h2);
+h2.innerText = h2.innerText + " from Visanth Designs"
+
+/* Qs2. Create 3 divs with common class name - "box".
+    Access them & add some unique text to each of them.*/
+
+let divs = document.querySelectorAll(".box");
+
+let idx = 1;
+for(div of divs) {
+    div.innerText = `New text ${idx}`;
+    idx++;
+}
